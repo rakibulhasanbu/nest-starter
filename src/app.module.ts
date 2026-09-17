35ca7901bc66a61dbd49011f9725b04a38a25b13
Rakibul Hasan <rakibulhasanbu@gmail.com>
2026-09-17 09:48:56 +0600

    refactor: remove AppController and AppService, clean up AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "@/app.controller";
-import { AppService } from "@/app.service";
 import { PrismaModule } from "@/database";
 import { HealthModule } from "@/modules/health/health.module";
 
 @Module({
     imports: [PrismaModule, HealthModule],
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
